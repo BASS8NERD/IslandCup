76a75a33d2e4341a6d994a1ffdcac36e1fd297cc
BASS8NERD <alkeas2@gmail.com>
2026-09-10 00:21:06 +0200

    app.py

--- app.py
+++ app.py
@@ -233,7 +233,7 @@ else:
             try:                
                 col_sx, col_cx, col_dx = st.columns([2.5, 1, 2.5])
                 with col_cx:
-                    st.image(creatura["immagine"], use_container_width=True)
+                    st.image(creatura["immagine"], width=70)
             except Exception:
                 st.write(f"🖼️ Creatura #{creatura['id']}")
         else:
