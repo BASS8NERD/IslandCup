918b89d49d3ac6bc658f59399084d0570b0b8411
BASS8NERD <alkeas2@gmail.com>
2026-09-09 23:09:54 +0200

    app.py

--- app.py
+++ app.py
@@ -37,11 +37,18 @@ st.markdown(
         margin-bottom: 15px;
         text-align: center;
         box-shadow: 0px 4px 6px rgba(0,0,0,0.05);
-    }    /* Centra le immagini delle creature */
-    div[data-testid="stImage"], div[data-testid="stImage"] > img {
+    }       /* Forza il contenitore invisibile a centrare l'immagine */
+    div[data-testid="element-container"]:has(img) {
+        display: flex !important;
+        justify-content: center !important;
+        align-items: center !important;
+        width: 100% !important;
+    }
+    div[data-testid="stImage"] img {
         display: block !important;
-        margin-left: auto !important;
-        margin-right: auto !important;
+        margin: 0 auto !important;
+    }
+
     }
     /* Centra il testo del Valore */
     div[data-testid="stMarkdownContainer"] {
