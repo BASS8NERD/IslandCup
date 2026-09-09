c6802ef4ac2d7e77d9521da2e5bfdf4fb6c92cf9
BASS8NERD <alkeas2@gmail.com>
2026-09-10 00:37:34 +0200

    app.py

--- app.py
+++ app.py
@@ -232,10 +232,8 @@ else:
         if creatura["immagine"] is not None:
             try:                
                 col_sx, col_cx, col_dx = st.columns()
-                with col_cx:
-                    st.markdown('<div style="text-align: center;">', unsafe_allow_html=True)
+                with col_cx:                    
                     st.image(creatura["immagine"], width=70)
-                    st.markdown('</div>', unsafe_allow_html=True)
             except Exception:
                 st.write(f"🖼️ Creatura #{creatura['id']}")
         else:
