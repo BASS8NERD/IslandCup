2818c1c434d51571a484fced49d7b6f38161f109
BASS8NERD <alkeas2@gmail.com>
2026-09-09 23:36:13 +0200

    app.py

--- app.py
+++ app.py
@@ -37,17 +37,10 @@ st.markdown(
         margin-bottom: 15px;
         text-align: center;
         box-shadow: 0px 4px 6px rgba(0,0,0,0.05);
-    }    /* Forza il contenitore invisibile di Streamlit al centro */
-    div[data-testid="element-container"]:has(img) {
-        display: flex !important;
-        justify-content: center !important;
+    }        .creature-card [data-testid="stVerticalBlock"] {
         align-items: center !important;
-        width: 100% !important;
     }
-    div[data-testid="stImage"] img {
-        display: block !important;
-        margin: 0 auto !important;
-    }
+
 
     /* Centra il testo del Valore */
     div[data-testid="stMarkdownContainer"] {
