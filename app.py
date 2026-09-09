277cf71f1afbd380cf0df4351e7ea3b7ae4d60f3
BASS8NERD <alkeas2@gmail.com>
2026-09-09 20:23:35 +0200

    app.py

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ with st.expander("🎮 ISOLANI"):
         with col_txt:
             nuovo_nome = st.text_input(f"Nome per {id_p}", value=st.session_state.nomi_giocatori[id_p], key=f"edit_{id_p}", label_visibility="collapsed")
             if nuovo_nome.strip():
-                st.session_state.nomi_giocatori[id_p] = nuevo_nome.strip()
+                st.session_state.nomi_giocatori[id_p] = nuovo_nome.strip()
     st.session_state.giocatori_attivi = partecipanti_scelti
 
 # 3. NUOVA ICONA: MARTELLO E CHIAVE INGLESE (Sfondo Verde Menta)
